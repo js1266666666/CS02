416cbb02d710375328a6b343c6942877bbfcd914
js1266666666 <127263671+js1266666666@users.noreply.github.com>
2023-03-15 10:45:28 +0900

    Add files via upload

---
+++ 006_BasicControls/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace _006_BasicControls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblname = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.btnclick = new System.Windows.Forms.Button();
+            this.lblhello = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblname
+            // 
+            this.lblname.AutoSize = true;
+            this.lblname.Location = new System.Drawing.Point(37, 53);
+            this.lblname.Name = "lblname";
+            this.lblname.Size = new System.Drawing.Size(33, 12);
+            this.lblname.TabIndex = 0;
+            this.lblname.Text = "이름:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(81, 50);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 21);
+            this.txtName.TabIndex = 1;
+            this.txtName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnclick
+            // 
+            this.btnclick.Location = new System.Drawing.Point(187, 50);
+            this.btnclick.Name = "btnclick";
+            this.btnclick.Size = new System.Drawing.Size(75, 23);
+            this.btnclick.TabIndex = 2;
+            this.btnclick.Text = "클릭";
+            this.btnclick.UseVisualStyleBackColor = true;
+            this.btnclick.Click += new System.EventHandler(this.btnclick_Click);
+            // 
+            // lblhello
+            // 
+            this.lblhello.AutoSize = true;
+            this.lblhello.Location = new System.Drawing.Point(79, 92);
+            this.lblhello.Name = "lblhello";
+            this.lblhello.Size = new System.Drawing.Size(0, 12);
+            this.lblhello.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 151);
+            this.Controls.Add(this.lblhello);
+            this.Controls.Add(this.btnclick);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblname);
+            this.Name = "Form1";
+            this.Text = "Basic Contorls";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblname;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Button btnclick;
+        private System.Windows.Forms.Label lblhello;
+    }
+}
+
---
+++ 006_BasicControls/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _006_BasicControls
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnclick_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text != "")
+                lblhello.Text = txtName.Text + "님! 안녕하세요!";
+            else
+                MessageBox.Show("이름을 입력하세요", "Warring");
+        }
+    }
+}
